5376a164a4d1645ee5e1374a30580733a89b290f
Falorun <nicolas.baudvin@gmail.com>
2020-04-06 14:52:45 +0200

    add unique validator

--- Models/user.js
+++ Models/user.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const UserSchema = mongoose.Schema({
     "username": { "type": String, "isRequired": true, "unique": true },
     "password": { "type": String, "isRequired": true },
     "email": { "type": String, "isRequired": true, "unique": true }
 });
+
+UserSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", UserSchema);
