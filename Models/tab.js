48cbfc8929e3b37571ba193633ded2df03409ba3
Falorun <nicolas.baudvin@gmail.com>
2020-04-07 14:46:43 +0200

    update models

--- Models/tab.js
+++ Models/tab.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const TabSchema = mongoose.Schema({
     "name": { "type": String, "isRequired": true },
     "socketId": { "type": String, "isRequired": true },
-    ""
+    "created_at": { "type": String, "isRequired": true }
 });
 
 module.exports = mongoose.model("Task", TabSchema);
