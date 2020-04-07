a550d15458cab8b05fbc2efe643d4f14d29bd4d7
Falorun <nicolas.baudvin@gmail.com>
2020-04-07 14:44:49 +0200

    check token from user

--- Controllers/SocketControllers/auth.js
+++ Controllers/SocketControllers/auth.js
@@ -1,3 +1,17 @@
-exports.identify = (token, socket) => {
-    console.log("Identification en cours de l'utilisateur...");
+const jwt = require("jsonwebtoken");
+
+exports.identify = (userData, socket) => {
+    const { token, userID } = userData;
+
+    console.log("Identification en cours de l'utilisateur...", token, userID);
+
+    const decoded = jwt.verify(token, process.env.SECRET_TOKEN_KEY);
+
+    if (decoded.userID !== userID) {
+        console.log("deconnexion de l'utilisateur => mauvaise identitée");
+        socket.emit("fail_identify");
+        return socket.disconnect();
+    }
+
+    return socket.emit("success_identify");
 };
