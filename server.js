e89895e1d461f67b3d8e1368acd4183704303b5f
Falorun <nicolas.baudvin@gmail.com>
2020-04-07 14:46:32 +0200

    i'm making test with socket io before real project implementation

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const http = require("http");
-const app = require("./app");
+const http = require("http"),
+    app = require("./app");
 
 const server = http.createServer(app);
 
@@ -56,12 +56,31 @@ server.listen(port);
 
 const io = require("socket.io")(server);
 const SocketAuthCtrl = require("./Controllers/SocketControllers/auth");
+const SocketTabCtrl = require("./Controllers/SocketControllers/tab");
+const url = require("url");
+const nsCreated = {};
+let matched = false;
 
 io.on("connection", (socket) => {
     console.log("un utilisateur s'est connecté au réseau");
-
     /**
      * Vérification de l'identité de l'utilisateur entrant
      */
-    socket.on("identify", (token) => SocketAuthCtrl.identify(token, socket));
+    socket.on("identify", (userData) => SocketAuthCtrl.identify(userData, socket));
+    // const { ns } = url.parse(socket.handshake.url, true).query;
+
+    // console.log(ns);
+    socket.on("new_tab", (tabData) => {
+        socket.join(tabData.name);
+    });
+
+    socket.on("room clients", (room) => {
+        console.log("vérification du nombre de client pour le channel", room);
+        // nombre de socket connecté au channel
+        console.log(io.sockets.adapter.rooms[room]);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("déconnexion d'un utilisateur");
+    });
 });
