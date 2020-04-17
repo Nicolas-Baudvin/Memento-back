175ee860c96ea9c64c135207efa7284102206c97
Falorun <nicolas.baudvin@gmail.com>
2020-04-17 14:14:51 +0200

    Mise en place des checker + middlewares

--- Routers/tab.routes.js
+++ Routers/tab.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const TabCtrl = require("../Controllers/tab");
+const auth = require("../Middlewares/auth");
+const checker = require("../Utils/tab");
 
 const router = express.Router();
 
-router.get("/:id", TabCtrl.find);
-router.post("/create/", TabCtrl.create);
-router.post("/update", TabCtrl.update);
-router.post("/delete/", TabCtrl.delete);
+router.get("/:id", auth, TabCtrl.find);
+router.post("/create/", [checker.create, auth], TabCtrl.create);
+router.post("/update", auth, TabCtrl.update);
+router.post("/delete/", [auth, checker.delete], TabCtrl.delete);
 
 module.exports = router;
