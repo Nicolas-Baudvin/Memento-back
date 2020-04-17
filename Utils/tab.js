1dbbd4d9b5cf1fe00973421cd9b656f4305b123f
Falorun <nicolas.baudvin@gmail.com>
2020-04-17 14:15:04 +0200

    ajout de checker

--- Utils/tab.js
+++ Utils/tab.js
@@ -1,7 +1,8 @@
 const { check } = require("express-validator");
 
 exports.create = [
-    check("name").notEmpty().withMessage("Tous les champs sont obligatoires")
+    check("name").notEmpty().withMessage("Tous les champs sont obligatoires"),
+    check("name").isLength({ "min": 1, "max": 30 }).withMessage("Le nom ne doit pas excéder 30 caractères")
 ];
 
 exports.find = [
