4fef30dace079d079e0a426d0e919f0dc8c45dae
Falorun <nicolas.baudvin@gmail.com>
2020-04-18 15:17:29 +0200

    correction mineure

--- Models/list.js
+++ Models/list.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ListSchema = mongoose.Schema({
     "name": { "type": String, "isRequired": true },
     "order": { "type": Number, "isRequired": true },
-    "userId": { "type": String, "isRequired": true },
+    "userID": { "type": String, "isRequired": true },
     "tabId": { "type": String, "isRequired": true }
 });
 
