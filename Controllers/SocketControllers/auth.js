7c041c7358f44898dbac7480480fe0fa5b3f434b
Falorun <nicolas.baudvin@gmail.com>
2020-04-23 14:00:42 +0200

    middleware plus robuste

--- Controllers/SocketControllers/auth.js
+++ Controllers/SocketControllers/auth.js
@@ -3,19 +3,19 @@ const jwt = require("jsonwebtoken");
 exports.identify = async (userData, socket) => {
 
     const { token, userID } = userData;
-
     try {
-        const decoded = jwt.verify(token, process.env.SECRET_TOKEN_KEY);
 
-        if (decoded.userID !== userID) {
-            console.log("deconnexion de l'utilisateur => mauvaise identitée");
-            socket.emit("fail_identify");
-            return socket.disconnect();
-        }
-        console.log("Connexion authentifiée");
-        return socket.emit("success identify");
+        jwt.verify(token, process.env.SECRET_TOKEN_KEY, (err, decoded) => {
+            if (decoded.userID !== userID) {
+                console.log("deconnexion de l'utilisateur => mauvaise identitée", err);
+                socket.emit("fail_identify", { err });
+                socket.leaveAll();
+                return socket.disconnect();
+            }
+            return socket.emit("success identify");
+        });
     } catch (e) {
-        throw new Error({ e, "errors": "Une erreur est survenue" });
+        throw new Error(e);
     }
 
 };
