e749bfc9707e6a0b882847e898253cfb7e2672cc
Falorun <nicolas.baudvin@gmail.com>
2020-04-27 15:32:04 +0200

    Mise à jour du model de la liste.

--- Models/list.js
+++ Models/list.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const ListSchema = mongoose.Schema({
     "name": { "type": String, "isRequired": true },
     "order": { "type": Number, "isRequired": true },
-    "userID": { "type": String, "isRequired": true },
     "tabId": { "type": String, "isRequired": true }
 });
 
