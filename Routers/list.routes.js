89bdbfa8760badd312a51f8e6d9d036d8f187cbf
Falorun <nicolas.baudvin@gmail.com>
2020-04-27 15:32:39 +0200

    passage de GET à POST pour la route /find/

--- Routers/list.routes.js
+++ Routers/list.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const listCtrl = require("../Controllers/list");
 const authMw = require("../Middlewares/auth");
-const checker = require("../Utils/tab");
+const checker = require("../Utils/list");
 
 const router = express.Router();
 
-router.get("/:id", [authMw, checker.find], listCtrl.find);
+router.post("/find/", [authMw, checker.find], listCtrl.find);
 router.post("/create/", [authMw, checker.create], listCtrl.create);
 router.post("/update", [authMw, checker.update], listCtrl.update);
 router.post("/delete/", [authMw, checker.delete], listCtrl.delete);
