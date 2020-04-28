5715d9cdfc0709855decff1b81dd7c32a249fd06
Falorun <nicolas.baudvin@gmail.com>
2020-04-28 13:43:09 +0200

    mise à jour du modèle Task

--- Models/task.js
+++ Models/task.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = mongoose.Schema({
-    "name": { "type": String, "isRequired": true },
-    "desc": { "type": String, "isRequied": true },
+    "title": { "type": String, "isRequired": true },
+    "desc": { "type": String, "isRequired": false },
     "order": { "type": String, "isRequired": true },
     "listId": { "type": String, "isRequired": true },
-    "labelId": { "type": Array, "isRequied": true }
+    "labelId": { "type": Array, "isRequired": false },
+    "tabId": { "type": String, "isRequired": true }
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
