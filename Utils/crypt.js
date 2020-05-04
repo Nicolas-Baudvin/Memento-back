e3ffc860bfa4fca690073dc770c403af8cba6a62
Falorun <nicolas.baudvin@gmail.com>
2020-05-04 13:38:12 +0200

    Les fonctions retournent désormais une promise

--- Utils/crypt.js
+++ Utils/crypt.js
@@ -4,16 +4,27 @@ const CryptoJS = require("crypto-js");
  * @param {Object} dataToDecrypt
  */
 exports.decryptUserData = (dataToDecrypt) => {
+    return new Promise((resolve, reject) => {
+        if (!dataToDecrypt || typeof dataToDecrypt === "undefined" || dataToDecrypt === null) {
+            return reject({ "err": "type Object Attendu" });
+        }
+        const bytes = CryptoJS.AES.decrypt(dataToDecrypt, process.env.SECRET_CRYPTO_KEY);
+        const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 
-    const bytes = CryptoJS.AES.decrypt(dataToDecrypt, process.env.SECRET_CRYPTO_KEY);
-    const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-
-    return decryptedData;
+        resolve({ decryptedData });
+    });
 };
 
 /**
  * @param {Object} dataToEncrypt
  */
 exports.cryptUserData = (dataToEncrypt) => {
-    return CryptoJS.AES.encrypt(JSON.stringify(dataToEncrypt), process.env.SECRET_CRYPTO_KEY).toString();
+    return new Promise((resolve, reject) => {
+        if (!dataToEncrypt || typeof dataToEncrypt === "undefined" || dataToEncrypt === null) {
+            return reject({ "err": "type String Attendu" });
+        }
+        const cryptedData = CryptoJS.AES.encrypt(JSON.stringify(dataToEncrypt), process.env.SECRET_CRYPTO_KEY).toString();
+
+        resolve({ cryptedData });
+    });
 };
