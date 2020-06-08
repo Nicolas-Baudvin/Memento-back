e89a95d4da23a84340bbef456b74d6e65571d8a5
unknown <nicolas.baudvin@gmail.com>
2020-06-08 20:10:22 +0200

    ajout middleware auth

--- Routers/fav.routes.js
+++ Routers/fav.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const favCtrl = require("../Controllers/fav");
 const router = express.Router();
+const authMw = require("../Middlewares/auth");
 
 
-router.post("/new-fav/", favCtrl.newFav);
-router.post("/delete-favs/", favCtrl.deleteFav);
-router.post("/get-favs/", favCtrl.getFav);
-router.post("/get-fav-tabs/", favCtrl.getFavTabs);
+router.post("/new-fav/", authMw, favCtrl.newFav);
+router.post("/delete-favs/", authMw, favCtrl.deleteFav);
+router.post("/get-favs/", authMw, favCtrl.getFav);
+router.post("/get-fav-tabs/", authMw, favCtrl.getFavTabs);
 
 module.exports = router;
