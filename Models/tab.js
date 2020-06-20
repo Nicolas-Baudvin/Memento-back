c49dd765c9f5fc233bf2d48dfe511bb8b199d3b2
unknown <nicolas.baudvin@gmail.com>
2020-06-20 20:09:56 +0200

    Ajout d'une propriété supplémentaire pour le lien d'image redimensionnée

--- Models/tab.js
+++ Models/tab.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const TabSchema = new mongoose.Schema({
     "name": { "type": String, "required": true },
     "socketRoomName": { "type": String, "required": false },
-    "created_at": { "type": String, "required": true },
     "imgPath": { "type": String, "required": true },
+    "resizedImgPath": { "type": String, "required": true },
     "userID": { "type": String, "required": true },
     "isPublic": { "type": Boolean, "required": false },
     "owner": { "type": String, "required": false }
-});
+}, { "timestamps": true });
 
 module.exports = mongoose.model("Tab", TabSchema);
