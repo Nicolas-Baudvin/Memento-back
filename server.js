c47eee429ed44fd2efed7fd665c42a996ca0c63f
Falorun <nicolas.baudvin@gmail.com>
2020-07-10 14:29:01 +0200

    WIP: Invitation liste d'amis en temps réel + notif si déconnecté

--- server.js
+++ server.js
@@ -59,6 +59,8 @@ const SocketAuthCtrl = require("./Controllers/SocketControllers/auth");
 const SocketTabCtrl = require("./Controllers/SocketControllers/tab");
 const SocketInvitationCtl = require("./Controllers/SocketControllers/invitation");
 const User = require("./Models/user");
+const Notifs = require("./Models/notifs");
+const Friends = require("./Models/friends");
 
 const roomCreated = {};
 
@@ -128,12 +130,87 @@ io.on("connection", (socket) => {
         if (to.socketID) {
             io.to(to.socketID).emit("invitation to be friend", ({ "from": from, "message": `${from.username} souhaite devenir votre ami` }));
         } else {
-            const user = await User.findOne({ "_id": to.from._id });
+            const newNotif = new Notifs({
+                "title": `${from.username} souhaite devenir votre ami`,
+                "from": from.username,
+                "userID": to._id
+            });
 
-            await User.updateOne(
-                { "_id": to.from._id },
-                { "notifs": [...user.notifs, { "from": from, "message": `${from.username} souhaite devenir votre ami` }] }
-            );
+            await newNotif.save();
+        }
+    });
+
+    socket.on("accept friend invitation", async (data) => {
+        const { owner, isFromNotif, userID } = data;
+
+        try {
+            const recipient = await User.findOne({ "_id": userID });
+            const transmitter = await User.findOne({ "username": owner });
+
+            const recipientFriends = await Friends.findOne({ "userID": recipient._id });
+            const transmitterFriends = await Friends.findOne({ "userID": transmitter._id });
+
+            if (!recipientFriends) {
+                const newRecipientFriends = new Friends({
+                    "userID": recipient._id,
+                    "list": [{ "username": transmitter.username }]
+                });
+
+                await newRecipientFriends.save();
+            } else {
+                const isAlreadyFriend = recipientFriends.list.filter((friend) => friend.username === transmitter.username).length;
+
+                if (!isAlreadyFriend) {
+                    recipientFriends.list = [...recipientFriends.list, { "username": transmitter.username }];
+                    await recipientFriends.save();
+                } else {
+                    io.to(transmitter.socketID).emit(
+                        "already friends",
+                        { "transmitter": recipient.username, "message": `Vous êtes déjà amis avec ${recipient.username}` }
+                    );
+                    return io.to(recipient.socketID).emit(
+                        "already friends",
+                        { "transmitter": transmitter.username, "message": `Vous êtes déjà amis avec ${transmitter.username}` }
+                    );
+                }
+            }
+            if (!transmitterFriends) {
+                const newTransmitterFriends = new Friends({
+                    "userID": transmitter._id,
+                    "list": [{ "username": recipient.username }]
+                });
+
+                await newTransmitterFriends.save();
+            } else {
+                const isAlreadyFriend = transmitterFriends.list.filter((friend) => friend.username === recipient.username).length;
+
+                if (!isAlreadyFriend) {
+                    transmitterFriends.list = [...transmitterFriends.list, { "username": recipient.username }];
+                    await transmitterFriends.save();
+                } else {
+                    return io.to(transmitter.socketID).emit(
+                        "already friends",
+                        { "transmitter": recipient.username, "message": `Vous êtes déjà amis avec ${recipient.username}` }
+                    );
+                }
+            }
+
+            if (isFromNotif) {
+                await Notifs.deleteMany({ "title": `${transmitter.username} souhaite devenir votre ami` });
+            }
+            const tFriends = await Friends.findOne({ "userID": transmitter._id });
+            const rFriends = await Friends.findOne({ "userID": recipient._id });
+
+            io.to(transmitter.socketID).emit("accept friend invitation", {
+                "msg": `${recipient.username} a accepté votre invitation, vous êtes désormais amis !`,
+                "friends": tFriends.list
+            });
+            io.to(recipient.socketID).emit("confirm accept invitation", {
+                "msg": `vous êtes désormais amis avec ${transmitter.username} !`,
+                "friends": rFriends.list
+            });
+        } catch (e) {
+            console.log(e);
         }
     });
 });
