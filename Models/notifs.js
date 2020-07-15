60bf6097d29a9d29bc5a52e4aec6e24e5a809274
Falorun <nicolas.baudvin@gmail.com>
2020-07-15 13:50:53 +0200

    Ajout d'une proprité de type bool pour savoir si c'est une notification qui requiert une action

--- Models/notifs.js
+++ Models/notifs.js
@@ -3,8 +3,9 @@ const timeZone = require("mongoose-timezone");
 
 const NotifSchema = new mongoose.Schema({
     "title": { "type": String, "required": true },
-    "from": { "type": String, "required": true },
-    "userID": { "type": String, "required": true }
+    "from": { "type": String || Boolean, "required": true },
+    "userID": { "type": String || Boolean, "required": true },
+    "isActionNotif": { "type": Boolean, "required": true } // display Accept button or not
 });
 
 NotifSchema.plugin(timeZone);
